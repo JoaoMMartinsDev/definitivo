d479129223c59b74e252f7589ab1d06be151cc34
JoaoMMartinsDev <joaomarcosma24@gmail.com>
2025-06-07 19:57:21 -0300

    Commit com a classe Animal criada

---
+++ src/main/java/com/petadota/model/Animal.java
@@ -0,0 +1,36 @@
+package com.petadota.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatorio")
+    private String nome;
+
+    @NotBlank(message = "O tipo é obrigatorio")
+    private String tipo;
+
+    @Positive (message = "A idade deve ser um número")
+    private Integer idade;
+
+    private String raca;
+
+    @NotBlank(message = "O status da adoção é obrigatório")
+    private String statusAdocao;
+
+    @Size(max = 500, message = "A descrição tem que ter no máximo 500 caracteres")
+    private String descricao;
+
+    private String imgUrl;
+
+}
